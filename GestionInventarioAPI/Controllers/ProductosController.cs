fb67ce2c7cc2762248a591940e173148e686383a
DanielBalza <danielbalza1999@gmail.com>
2026-04-14 16:03:39 -0500

    Agregar archivos de proyecto.

---
+++ GestionInventarioAPI/Controllers/ProductosController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GestionInventarioAPI.Models; // Importamos nuestro modelo de producto
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GestionInventarioAPI.Controllers
+{
+    [Route("api/[controller]")] //La ruta sera: api/productos
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        //creamos una lista estatica para simular una base de datos por ahora
+        private static List<Producto> productos = new List<Producto>
+        {
+            new Producto { Id = 1, Nombre = "Laptop Gamer", Precio = 1200, Stock = 5 },
+            new Producto { Id = 2, Nombre = "mause Óptico", Precio = 25, Stock = 50 }
+        };
+
+        /// <summary>
+        ///    obtiene la lista completa de productos
+        /// </summary>
+        // GET: api/<ProductosController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Producto>> Get()
+        {
+            return Ok(productos); // Devuelve la lista de productos con un código 200 OK
+        }
+
+        /// <summary>
+        /// Obtiene un producto especifico por su ID. Si el producto no existe, devuelve un código 404 Not Found.
+        /// </summary>
+
+        // GET api/<ProductosController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Producto> Get (int id)
+        {
+            var producto = productos.FirstOrDefault(p => p.Id == id); // Busca el producto por ID
+
+            if (producto == null)
+            {
+                return NotFound(); // Buena practica: si no existe, avisamos al usuario (Error 404)
+            }
+            return Ok(producto);
+        }
+
+        // POST api/<ProductosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GestionInventarioAPI/Models/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionInventarioAPI.Models
+{
+    public class Producto
+    {
+        // El ID es vital ara identificar cada registro de forma unica
+        public int Id { get; set; }
+        // El nombre del producto es esencial para su identificación y descripción
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
